61733de8929bf983380827290bd8493cbad2c113
mitzuh <36733151+mitzuh@users.noreply.github.com>
2018-06-11 16:04:58 +0300

    Main menu
    Made main menu

---
+++ ApproSimulator/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quitgame()
+    {
+        Application.Quit();
+    }
+
+    private void OnMouseEnter()
+    {
+
+    }
+}

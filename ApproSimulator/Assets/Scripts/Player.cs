595b8100210eaa03b40e901b083f2f4ed3491774
Muskotti <toni.vanttinen@dnainternet.net>
2018-06-11 17:23:06 +0300

    Players drunk movement
    new version of drunk movement

--- ApproSimulator/Assets/Scripts/Player.cs
+++ ApproSimulator/Assets/Scripts/Player.cs
@@ -3,28 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
-
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
     private Boolean facingRight;
     private float delta;
+    private float timer;
+    private float randomPushVer;
+    private float randomPushHor;
 
-    public float speed;
-    public float drunkness;
-    public float amplitude;
+    //public float speed;
+    //public float drunkness;
+    //public float amplitude;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("DrunkAutoMove", 0.5f,0.5f);
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         Move();
-	}
+    }
 
     private void Move()
     {
         delta = Time.deltaTime;
-       
+
         if (Input.GetKey(KeyCode.D))
         {
             if (!facingRight)
@@ -32,7 +40,7 @@ public class Player : MonoBehaviour {
                 transform.Rotate(new Vector3(0, 180, 0));
                 facingRight = true;
             }
-            transform.position += Vector3.right * delta * (Mathf.Sin(drunkness) * amplitude);
+            transform.position += Vector3.right * delta;
         }
 
         if (Input.GetKey(KeyCode.A))
@@ -42,17 +50,24 @@ public class Player : MonoBehaviour {
                 transform.Rotate(new Vector3(0, -180, 0));
                 facingRight = false;
             }
-            transform.position -= Vector3.right * delta * (Mathf.Sin(drunkness) * amplitude);
+            transform.position -= Vector3.right * delta;
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.up * delta * (Mathf.Sin(drunkness) * amplitude);
+            transform.position += Vector3.up * delta;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.down * delta * (Mathf.Sin(drunkness) * amplitude);
+            transform.position += Vector3.down * delta;
         }
+    }
+
+    private void DrunkAutoMove()
+    {
+        randomPushVer = UnityEngine.Random.Range(-5f, 5f);
+        randomPushHor = UnityEngine.Random.Range(-5f, 5f);
+        rb.AddForce(new Vector2(randomPushHor, randomPushVer));
     }
 }

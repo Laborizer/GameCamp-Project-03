81520f2cc5a3dd2a798b1f47d44b48328124c12a
mitzuh <36733151+mitzuh@users.noreply.github.com>
2018-06-12 19:53:24 +0300

    Bar door
    Made door to bar

---
+++ ApproSimulator/Assets/Scripts/Bar1Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bar1Door : MonoBehaviour {
+
+    bool canEnter;
+
+	// Use this for initialization
+	void Start () {
+        canEnter = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(canEnter && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene("Bar1");
+        }
+
+        canEnter = false;
+	}
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            canEnter = true;
+        }
+    }
+}

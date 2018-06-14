3e8e554c3a81daba8a39a11901bb918d7e6eff3d
mitzuh <36733151+mitzuh@users.noreply.github.com>
2018-06-14 16:58:50 +0300

    Enemy
    Made an enemy npc, who chases player

---
+++ ApproSimulator/Assets/Scripts/EnemyNPC.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyNPC : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, GameObject.Find("Player").transform.position, (Time.deltaTime/2));
+    }
+
+}

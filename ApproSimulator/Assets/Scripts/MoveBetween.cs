f585afea8cbdc6f1a80a68ed4b61593a478b48da
Muskotti <toni.vanttinen@dnainternet.net>
2018-06-18 19:06:53 +0300

    added car
    made a car that moves between 2 points

---
+++ ApproSimulator/Assets/Scripts/MoveBetween.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBetween : MonoBehaviour {
+
+
+    public GameObject moveObject;
+    public GameObject pointA;
+    public GameObject pointB;
+    public float Speed;
+
+    private Vector2 startPos;
+    private Vector2 endPos;
+    private float currentLerpTime = 0;
+    
+    void Start()
+    {
+        startPos = pointA.transform.position; 
+        endPos = pointB.transform.position;
+    }
+    void Update()
+    {
+        moveObject.transform.position = Vector2.Lerp(startPos,endPos, Mathf.PingPong(Time.time * Speed, 1.0f));
+    }
+}

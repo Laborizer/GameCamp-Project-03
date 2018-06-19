c3764f68c2f7e174381a7db86c6ed8646fb22750
Muskotti <toni.vanttinen@dnainternet.net>
2018-06-19 15:52:34 +0300

    Camera fix
    Fixed cameras position when swaping scenes

--- ApproSimulator/Assets/Scripts/CameraFollow.cs
+++ ApproSimulator/Assets/Scripts/CameraFollow.cs
@@ -8,14 +8,12 @@ public class CameraFollow : MonoBehaviour {
 
     void Start()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        transform.position = target.position - new Vector3(0,0,10);
         offset = transform.position - target.position;
     }
 
-    // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         transform.position = target.position + offset;
     }
 }

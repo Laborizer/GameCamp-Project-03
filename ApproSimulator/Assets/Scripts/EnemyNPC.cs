f7e654a60f74b04fa179da9c6c923bdb82e33327
mitzuh <36733151+mitzuh@users.noreply.github.com>
2018-06-20 19:37:13 +0300

    Sounds and music
    Made sounds and music

--- ApproSimulator/Assets/Scripts/EnemyNPC.cs
+++ ApproSimulator/Assets/Scripts/EnemyNPC.cs
@@ -4,20 +4,26 @@ using UnityEngine.SceneManagement;
 
 public class EnemyNPC : MonoBehaviour
 {
+    AudioSource audiosource;
+    public AudioClip getOut;
 
     GameObject player;
     GameObject door;
 
     public bool canGrapple;
+    bool soundPlayed;
     public string debug;
 
     void Start()
     {
+        audiosource = GetComponent<AudioSource>();
+
         player = GameObject.Find("Player");
         door = GameObject.Find("Game");
 
         player.GetComponent<Player>().transform.parent = null;
         canGrapple = false;
+        soundPlayed = false;
     }
 
     void Update()
@@ -46,9 +52,11 @@ public class EnemyNPC : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D obj)
     {
-        if (obj.gameObject.tag == "Player")
+        if (obj.gameObject.tag == "Player" && !soundPlayed)
         {
+            audiosource.PlayOneShot(getOut, 2f);
             canGrapple = true;
+            soundPlayed = true;
         }
     }
 
